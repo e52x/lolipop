bb26739246648039d259c2538da0267d6e4b300a
KangSantuy <pancasyah191@gmail.com>
2022-09-30 01:47:22 +0700

    Add files via upload

---
+++ lolipop.py
@@ -0,0 +1,7 @@
+import phonenumbers
+from phonenumbers import geocoder
+from phonenumbers import carrier
+# "MASUK KE CODE PYTHON DULU CUY"  
+#CONTOH  NANO LOLIPOP.PY DAN ISI NOMOR TAGET DI Parse"
+print(geocoder.description_for_valid_number(phonenumbers.parse("Masukan Nomor Hp Taget"),"en"))
+print(carrier.name_for_number(phonenumbers.parse(" Masukan Nomor Hp Taget"),"en"))
