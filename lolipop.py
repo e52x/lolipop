00849a4bbd4aab981c979b7058e438ad3566e38c
KangSantuy <pancasyah191@gmail.com>
2022-11-01 16:50:55 +0700

    Update lolipop.py

--- lolipop.py
+++ lolipop.py
@@ -2,6 +2,6 @@ import phonenumbers
 from phonenumbers import geocoder
 from phonenumbers import carrier
 # "MASUK KE CODE PYTHON DULU CUY"  
-#CONTOH  NANO LOLIPOP.PY DAN ISI NOMOR TAGET DI Parse"
+#CONTOH  NANO LOLIPOP.PY DAN ISI NOMOR TARGET DI Parse"
 print(geocoder.description_for_valid_number(phonenumbers.parse("Masukan Nomor Hp Taget"),"en"))
 print(carrier.name_for_number(phonenumbers.parse(" Masukan Nomor Hp Taget"),"en"))
